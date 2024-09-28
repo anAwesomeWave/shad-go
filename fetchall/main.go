f0bbbac2483c81d7a44dabde1f349a9b8f834a23
Timofey Zubov <timofey.zubov.05@gmail.com>
2024-09-28 22:18:56 +0300

    solved fetchall

--- fetchall/main.go
+++ fetchall/main.go
@@ -2,6 +2,34 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
 
+func fetch(url string, c chan struct{}) {
+	start := time.Now()
+	_, err := http.Get(url)
+	if err != nil {
+		log.Printf("%v err: %v", url, err)
+	}
+	log.Printf("%v\t%v\n", time.Since(start), url)
+	c <- struct{}{}
+}
+
+func main() {
+	urls := os.Args[1:]
+	start := time.Now()
+	c := make(chan struct{})
+
+	for _, url := range urls {
+		go fetch(url, c)
+	}
+	for range urls {
+		<-c
+	}
+	fmt.Printf("Program ended with total time: %v", time.Since(start))
 }
