338af4014e13ca852a959f9fb7f9d035438ca377
Timofey Zubov <timofey.zubov.05@gmail.com>
2024-09-28 21:45:19 +0300

    urlfetch solved

--- urlfetch/main.go
+++ urlfetch/main.go
@@ -2,6 +2,27 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
+func main() {
+	args := os.Args[1:]
+	for _, url := range args {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Printf("%v\tfor url:%v\n", err, url)
+			os.Exit(1)
+		}
+		body, err := io.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Printf("%v\tfor url:%v\n", err, url)
+			os.Exit(2)
+		}
+		fmt.Printf("%v\n", string(body))
+	}
 }
