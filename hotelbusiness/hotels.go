5789c7cd5c226c901ed71878962f9a5605d74442
Timofey Zubov <timofey.zubov.05@gmail.com>
2024-10-06 19:57:16 +0300

    solve hotel

--- hotelbusiness/hotels.go
+++ hotelbusiness/hotels.go
@@ -13,5 +13,23 @@ type Load struct {
 }
 
 func ComputeLoad(guests []Guest) []Load {
-	return []Load{}
+
+	dates := make(map[int]int)
+	maxDate := 0
+	for _, v := range guests {
+		maxDate = max(maxDate, v.CheckInDate, v.CheckOutDate)
+		dates[v.CheckInDate]++
+		dates[v.CheckOutDate]--
+	}
+	curAns := 0
+	var ans []Load
+	for i := 0; i <= maxDate; i++ {
+		if dates[i] == 0 {
+			continue
+		}
+		curAns += dates[i]
+		ans = append(ans, Load{i, curAns})
+	}
+
+	return ans
 }
