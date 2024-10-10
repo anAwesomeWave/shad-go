7806ed173ddb29e86ab506224574b842d94af578
Timofey Zubov <timofey.zubov.05@gmail.com>
2024-10-10 13:11:29 +0300

    solved spacecollapse with 1 alloc

--- utf8/spacecollapse/collapse.go
+++ utf8/spacecollapse/collapse.go
@@ -2,6 +2,25 @@
 
 package spacecollapse
 
+import (
+	"strings"
+	"unicode/utf8"
+)
+
 func CollapseSpaces(input string) string {
-	return ""
+	var ans strings.Builder
+	ans.Grow(len(input))
+	isLastRuneSpace := false
+	for i := 0; i < len(input); {
+		nextRune, sz := utf8.DecodeRuneInString(input[i:])
+		i += sz
+		if strings.ContainsRune("\t\n\r", nextRune) {
+			nextRune = ' '
+		}
+		if !(isLastRuneSpace && nextRune == ' ') {
+			ans.WriteRune(nextRune)
+		}
+		isLastRuneSpace = nextRune == ' '
+	}
+	return ans.String()
 }
