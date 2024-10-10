8418eeecc1c9c7f08767dd6217481e81c0923396
Timofey Zubov <timofey.zubov.05@gmail.com>
2024-10-10 12:44:30 +0300

    return to utf8 package.

--- utf8/reverse/reverse.go
+++ utf8/reverse/reverse.go
@@ -4,14 +4,17 @@ package reverse
 
 import (
 	"strings"
+	"unicode/utf8"
 )
 
 func Reverse(input string) string {
 	var ans strings.Builder
 	ans.Grow(len(input))
-	runes := []rune(input)
-	for i := len(runes) - 1; i >= 0; i-- {
-		ans.WriteRune(runes[i])
+	i := len(input) - 1
+	for i >= 0 {
+		r, sz := utf8.DecodeLastRuneInString(input[:i+1])
+		i -= max(1, sz)
+		ans.WriteRune(r)
 	}
 	return ans.String()
 }
