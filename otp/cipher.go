37391c9c15cef7d4cec12da256afd503d679782a
Timofey Zubov <timofey.zubov.05@gmail.com>
2024-11-06 21:21:09 +0300

    add otp

--- otp/cipher.go
+++ otp/cipher.go
@@ -6,10 +6,62 @@ import (
 	"io"
 )
 
+type XorReader struct {
+	r    io.Reader
+	prng io.Reader
+}
+
+func (xr *XorReader) Read(p []byte) (int, error) {
+	sz := len(p)
+
+	// принимает слайс p, декодирует n <= len(p) байт из потока r
+
+	buf := make([]byte, sz)
+	sz, err := xr.r.Read(buf)
+
+	prng := make([]byte, sz)
+	_, _ = xr.prng.Read(prng)
+
+	for i := 0; i < sz; i++ {
+		p[i] = buf[i] ^ prng[i]
+	}
+
+	return sz, err
+
+}
+
+type XorWriter struct {
+	w    io.Writer
+	prng io.Reader
+}
+
+func (xw *XorWriter) Write(p []byte) (int, error) {
+	sz := len(p)
+	prng := make([]byte, sz)
+	_, _ = xw.prng.Read(prng)
+	encoded := make([]byte, len(p))
+	for i := 0; i < sz; i++ {
+		encoded[i] = p[i] ^ prng[i]
+	}
+	n, err := xw.w.Write(encoded)
+	if n != sz || err != nil {
+		return n, err
+	}
+	return sz, nil
+}
+
 func NewReader(r io.Reader, prng io.Reader) io.Reader {
-	panic("implement me")
+	// хотим создать reader
+	// будет декодировать p байт
+	return &XorReader{
+		r,
+		prng,
+	}
 }
 
 func NewWriter(w io.Writer, prng io.Reader) io.Writer {
-	panic("implement me")
+	return &XorWriter{
+		w,
+		prng,
+	}
 }
