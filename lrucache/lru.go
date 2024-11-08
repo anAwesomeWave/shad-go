f2353473ce5445911ea523149870ae55905562f1
Timofey Zubov <timofey.zubov.05@gmail.com>
2024-11-08 10:56:26 +0300

    add lru with lib list

--- lrucache/lru.go
+++ lrucache/lru.go
@@ -2,83 +2,12 @@
 
 package lrucache
 
+import "container/list"
+
 type LRUCache struct {
-	head      *node // cannot convert to diff type. need map in internal funcs
-	tail      *node
-	keyToNode map[int]*node
+	list      *list.List
+	keyToNode map[int]*list.Element
 	cap       int
-}
-
-type node struct {
-	key, data   int
-	left, right *node
-}
-
-func (lc *LRUCache) addLast(key, x int) {
-
-	newNode := &node{key: key, data: x, left: nil, right: nil}
-	switch {
-	case lc.head == nil:
-		// first node
-		lc.head = newNode
-		lc.tail = newNode
-		lc.keyToNode[key] = lc.head
-	case lc.tail == lc.head:
-		// second node. head != nil
-		lc.tail = newNode
-		lc.tail.left = lc.head
-		lc.head.right = lc.tail
-		lc.keyToNode[key] = lc.tail
-	default:
-		lc.tail.right = newNode
-		newNode.left = lc.tail
-		lc.tail = newNode
-		lc.keyToNode[key] = lc.tail
-	}
-
-}
-
-func (lc *LRUCache) popFront() bool {
-	switch {
-	case lc.head == nil:
-		return false
-	case lc.head == lc.tail:
-		key := lc.head.key
-		delete(lc.keyToNode, key)
-		lc.head = nil
-		lc.tail = nil
-	default:
-		key := lc.head.key
-		delete(lc.keyToNode, key)
-		lc.head = lc.head.right
-	}
-	return true
-}
-
-func (lc *LRUCache) nodeToFront(nd *node) *int {
-	// replace and return it's value
-	if nd == nil {
-		return nil
-	}
-	switch {
-	case nd == lc.head:
-		if nd != lc.tail {
-			lc.head = lc.head.right
-			lc.tail.right = nd
-			nd.left = lc.tail
-			lc.tail = nd
-		}
-	case nd == lc.tail:
-		// tail != head
-	default:
-		nd.left.right = nd.right
-		nd.right.left = nd.left
-		lc.tail.right = nd
-		nd.right = nil
-		nd.left = lc.tail
-		lc.tail = nd
-	}
-	return &nd.data
 }
 
 func (lc *LRUCache) Get(key int) (int, bool) {
@@ -86,27 +15,36 @@ func (lc *LRUCache) Get(key int) (int, bool) {
 	if !isPresent || node == nil {
 		return 0, false
 	}
-	lc.nodeToFront(node)
-	return node.data, true
+
+	lc.list.MoveToBack(node)
+	val := node.Value.([2]int)
+	return val[1], true
 }
 
 func (lc *LRUCache) Set(key, value int) {
 	if _, found := lc.Get(key); found {
-		lc.keyToNode[key].data = value
-		//lc.nodeToFront(lc.keyToNode[key])
+		lc.keyToNode[key].Value = [2]int{key, value}
+		lc.list.MoveToBack(lc.keyToNode[key])
 	} else {
 		if len(lc.keyToNode) >= lc.cap {
-			if !lc.popFront() {
+			if lc.list.Front() != nil {
+				ptr := lc.list.Front()
+				data := ptr.Value.([2]int)
+				delete(lc.keyToNode, data[0])
+				lc.list.Remove(ptr)
+			} else {
 				return
 			}
 		}
-		lc.addLast(key, value)
+		ptr := lc.list.PushBack([2]int{key, value})
+		lc.keyToNode[key] = ptr
 	}
 }
 
 func (lc *LRUCache) Range(f func(key, value int) bool) {
-	for node := lc.head; node != nil; node = node.right {
-		if cont := f(node.key, node.data); !cont {
+	for node := lc.list.Front(); node != nil; node = node.Next() {
+		val := node.Value.([2]int)
+		if cont := f(val[0], val[1]); !cont {
 			break
 		}
 	}
@@ -114,15 +52,17 @@ func (lc *LRUCache) Range(f func(key, value int) bool) {
 
 func (lc *LRUCache) Clear() {
 	clear(lc.keyToNode)
-	for lc.head != nil {
-		lc.popFront()
+	for lc.list.Len() > 0 {
+		lc.list.Remove(lc.list.Back())
 	}
 }
 
 func New(cap int) Cache {
-	cache := LRUCache{}
-	cache.keyToNode = make(map[int]*node, cap)
-	cache.cap = cap
+	cache := LRUCache{
+		keyToNode: make(map[int]*list.Element, cap),
+		cap:       cap,
+		list:      list.New(),
+	}
 	return &cache
 }
 
