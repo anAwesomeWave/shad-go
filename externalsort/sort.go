7783f4e86059a568c8fd4fed4e4940c417b92efc
Timofey Zubov <timofey.zubov.05@gmail.com>
2024-11-09 17:55:33 +0300

    implement externalsort. Start passing readers to heap instead of all lines

--- externalsort/sort.go
+++ externalsort/sort.go
@@ -5,6 +5,7 @@ package externalsort
 import (
 	"container/heap"
 	"io"
+	"os"
 	"sort"
 )
 
@@ -22,7 +23,7 @@ func (lineR MyLReader) ReadLine() (string, error) {
 		if byteCnt != 0 && charSlice[0] != '\n' {
 			data = append(data, charSlice...)
 		}
-		if byteCnt == 0 || readErr == io.EOF || charSlice[0] == '\n' {
+		if byteCnt == 0 || readErr != nil || charSlice[0] == '\n' {
 			break
 		}
 	}
@@ -47,19 +48,24 @@ func NewWriter(w io.Writer) LineWriter {
 	return MyLWriter{w: w}
 }
 
-type StrHeap []string
-
-func (h StrHeap) Len() int           { return len(h) }
-func (h StrHeap) Less(i, j int) bool { return h[i] < h[j] }
-func (h StrHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-
-func (h *StrHeap) Push(x any) {
-	// Push and Pop use pointer receivers because they modify the slice's length,
-	// not just its contents.
-	*h = append(*h, x.(string))
+type DataReader struct {
+	line   string
+	reader LineReader
 }
 
-func (h *StrHeap) Pop() any {
+type Heap []*DataReader
+
+func (h Heap) Len() int           { return len(h) }
+func (h Heap) Less(i, j int) bool { return h[i].line < h[j].line }
+func (h Heap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *Heap) Push(x any) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*h = append(*h, x.(*DataReader))
+}
+
+func (h *Heap) Pop() any {
 	old := *h
 	n := len(old)
 	x := old[n-1]
@@ -71,23 +77,32 @@ func Merge(w LineWriter, readers ...LineReader) error {
 	/*
 		каждый line reader посорчен. этап сортировки всех посл. в 1
 	*/
-	//allStrs := make([]string, 0, len(readers)) // cap min -> len(readers)
-	strHeap := StrHeap{}
+	pq := Heap{}
 	for _, r := range readers {
-		for { // eof -> end
-			str, rErr := r.ReadLine()
-			//allStrs = append(allStrs, str)
-			heap.Push(&strHeap, str)
-			if rErr != nil {
-				break
-			}
+		line, err := r.ReadLine()
+		if err == io.EOF && len(line) == 0 {
+			continue
 		}
+		heap.Push(&pq, &DataReader{line: line, reader: r})
+
 	}
-	for len(strHeap) > 0 {
-		elem := heap.Pop(&strHeap).(string)
-		if err := w.Write(elem); err != nil {
+	for len(pq) > 0 {
+		elem := heap.Pop(&pq).(*DataReader)
+
+		if err := w.Write(elem.line); err != nil {
 			return err
 		}
+		newLine, err := elem.reader.ReadLine()
+
+		if err != nil && err != io.EOF {
+			return err
+		}
+
+		if err == io.EOF && len(newLine) == 0 {
+			continue
+		}
+		elem.line = newLine
+		heap.Push(&pq, elem)
 	}
 	return nil
 }
@@ -96,12 +111,58 @@ func Sort(w io.Writer, in ...string) error {
 	/*
 		получаем файлы. их превращаем в line readers сортируем между собой и отдаем в merge
 	*/
-	sort.Strings(in)
-	for _, str := range in {
-		_, err := w.Write([]byte(str))
+	// 1. get file
+	// 2. sort
+	// 3. remove from memo
+	for _, filePath := range in {
+
+		rfile, err := os.OpenFile(filePath, os.O_RDONLY, 7555)
+
 		if err != nil {
 			return err
 		}
+		defer rfile.Close()
+
+		lReader := NewReader(rfile)
+		data := []string{}
+		for {
+			newStr, err := lReader.ReadLine()
+			if err == io.EOF && len(newStr) == 0 {
+				break
+			}
+			data = append(data, newStr)
+		}
+		if err := rfile.Close(); err != nil {
+			return err
+		}
+		if len(data) == 0 {
+			continue
+		}
+		wfile, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE, 0755)
+		if err != nil {
+			return err
+		}
+		defer wfile.Close()
+		sort.Slice(data, func(i, j int) bool { return data[i] < data[j] })
+		lfWriter := NewWriter(wfile)
+		for _, str := range data {
+			if err := lfWriter.Write(str); err != nil {
+				return err
+			}
+		}
+		wfile.Close()
 	}
-	return nil
+	// 4. merge all
+	readers := make([]LineReader, 0, len(in))
+	for _, filePath := range in {
+		file, err := os.OpenFile(filePath, os.O_RDONLY, 'r')
+		defer file.Close()
+		if err != nil {
+			return err
+		}
+		readers = append(readers, NewReader(file))
+	}
+	lWriter := NewWriter(w)
+	return Merge(lWriter, readers...)
+
 }
